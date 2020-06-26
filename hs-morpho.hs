1b5227c155e769c2cd3887214f1b095458d8fe6b
anton-shrdlu <mail@antonhampe.de>
2020-06-26 13:18:16 +0200

    added wrapper functions for merge and move

--- hs-morpho.hs
+++ hs-morpho.hs
@@ -1,6 +1,9 @@
-import Fst
+-- import Fst
 import Data.List (inits,tails,delete,nub)
 import Control.Monad (guard)
+import Data.Function ((&))
+import Data.Either (rights)
+import Data.Bifunctor (Bifunctor(first))
 -- import Control.Lens
 
 {-
@@ -21,22 +24,28 @@ The Exponent is the most specic item that satisfies compatability
 
 data Feature = Binary String Bool deriving (Eq, Show)
 -- add valued Features
-data Category = N | V | A -- Categories could just be Strings?
+data Category = N | V | A deriving Eq-- Categories could just be Strings?
 
 type Morpheme  = Either Stem Exponent
-data Exponent  = Exponent String [Feature]
-data Array     = Array [Exponent]
-data Stem      = Stem Category String -- unsure if Category is needed.
-data Workspace = Workspace [Feature] [Array] [Morpheme]
+data Exponent  = Exponent String [Feature] deriving Eq
+data Array     = Array {exponents :: [Exponent]} deriving Eq
+data Stem      = Stem String Category deriving Eq -- unsure if Category is needed.
+data Workspace = Workspace [Feature] [Array] [Morpheme] deriving Eq
 {-
 Constraints could be DFSTs that target one of the three lists in the Workspace.
 -}
 mkWorkspace :: Either Stem Exponent -> [Feature] -> [Array] -> Workspace
-mkWorkspace stem@(Left (Stem category string)) features arrays = 
+mkWorkspace stem@(Left (Stem string category)) features arrays = 
     Workspace features arrays [stem]
 
 split :: [a] -> [([a], [a])]
 split xs = zip (inits xs) (tails xs)
+
+wrapMerge :: Workspace -> [Workspace]
+wrapMerge (Workspace features arrays morphemes) =
+    merge (map (map Right . exponents) arrays,morphemes)
+    & map (first (map (Array . rights)))
+    & map (uncurry (Workspace features))
 
 merge :: Eq a => ([[a]], [a]) -> [([[a]], [a])]
 merge (arrays,workspace) = 
@@ -47,6 +56,10 @@ merge (arrays,workspace) =
         let (array,symbol') = symbol <$ taggedArray
         (work,space) <- split workspace
         return (array, work ++ (symbol' : space)) 
+
+wrapMove :: Workspace -> [Workspace]
+wrapMove (Workspace features arrays morphemes) =
+    map (Workspace features arrays) $ move morphemes
 
 move :: Eq a => [a] -> [[a]]
 move workspace =
