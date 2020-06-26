83eb5d2d976db8f4d123b475613c089174a48e6c
anton-shrdlu <mail@antonhampe.de>
2020-06-27 01:54:01 +0200

    added features, array, constraints and ranking for first grammar

--- exampleGrammars.hs
+++ exampleGrammars.hs
@@ -2,3 +2,44 @@ import HS_morpho
 
 gen :: GEN
 gen = mkGen True False
+
+obl :: Bool -> Feature
+obl = Binary "obl"
+gov :: Bool -> Feature
+gov = Binary "gov"
+masc :: Bool -> Feature
+masc = Binary "masc"
+fem :: Bool -> Feature
+fem = Binary "fem"
+
+k :: [Array]
+k = pure
+    [ Exponent "m" [masc True, obl True, gov True]
+    , Exponent "s" [masc True, obl True]
+    , Exponent "s" [masc True, fem True]
+    , Exponent "n" [masc True, gov True]
+    , Exponent "r" [masc True]
+    , Exponent "r" [masc True]
+    , Exponent "n" [obl True, gov True]
+    , Exponent "r" [obl True]
+    , Exponent "e" []
+    ]
+
+diesRanking :: Ranking
+diesRanking = 
+    [ mc
+    , idF
+    , maxM
+    , maxO
+    , maxF
+    , maxG
+    ]
+
+maxM :: Constraint
+maxM = mkMax "masc"
+maxO :: Constraint
+maxO = mkMax "obl"
+maxF :: Constraint
+maxF = mkMax "fem"
+maxG :: Constraint
+maxG = mkMax "gov"
